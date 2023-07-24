6e365a60c6bf0fbab6bcefc97dd75e1534e2dba2
Oguzhan Aydin <moritanyali26@yahoo.com>
2023-07-24 22:00:11 +0300

    US001_V1

---
+++ src/test/java/tests/US001/TC01_AramaKutusuTesti.java
@@ -0,0 +1,16 @@
+package tests.US001;
+
+import org.testng.annotations.Test;
+import pages.Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC01_AramaKutusuTesti {
+
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        Page page = new Page();
+        //blabla
+    }
+}
